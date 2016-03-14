152c31b3f7c0a7e80a52d21f3af3b83eecca7123
Kenji Crosland <kenji.crosland@gmail.com>
2016-03-13 19:27:07 -0700

    Update Habit schema

--- models/habit.js
+++ models/habit.js
@@ -2,7 +2,22 @@ var mongoose = require('mongoose');
 
 var habitSchema = new mongoose.Schema({
   name: String,
-  pointValue: Number
+  //Start Date and End Date for the bonus interval period.
+  startDate: Date,
+  endDate: Date,
+  pointValue: Number,
+  //Bonus interval: Daily, Weekly, Monthly
+  bonusInterval: String,
+  bonusFrequency: Number,
+  intervals: [{
+    intervalStart: Date,
+    intervalEnd: Date,
+    allComplete: Boolean,
+    completions: [{
+      completedOn: Date,
+      pointValue: Number
+    }]
+  }]
 });
 
 module.exports = mongoose.model('Habit', habitSchema);
