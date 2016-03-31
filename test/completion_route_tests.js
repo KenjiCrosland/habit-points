185a497848a5958981afca6113279a283a81d749
Kenji Crosland <kenji.crosland@gmail.com>
2016-03-30 19:24:36 -0700

    Add more completion route tests

--- test/completion_route_tests.js
+++ test/completion_route_tests.js
@@ -58,41 +58,64 @@ describe('completion POST routes', function(){
   });
 
   describe('a habit with an existing interval', function(){
+    var testHabit;
+    beforeEach(function(){
+     return new Habit({
+      name: 'Drink a glass of water',
+      pointValue: 1,
+      bonusInterval: 'day',
+      bonusFrequency: 4,
+      intervals:[{
+        allComplete: false,
+        intervalEnd: moment().endOf('day').toDate(),
+        intervalStart: moment().startOf('day').toDate(),
+        completions: [{completedOn: Date.now()}, {completedOn: Date.now()}]
+      }]
+    }).save().then(function(habit){
+      testHabit = habit;
+    })
+  });
 
-    before(function(done){
-      var date = moment(new Date()).add(2, 'days').toDate();
-      var testHabit;
-
-      (new Habit({
-        name: 'Drink a glass of water',
-        pointValue: 1,
-        bonusInterval: 'day',
-        intervals:[{
-          allComplete: false,
-          intervalEnd: moment().endOf('day').toDate(),
-          intervalStart: moment().startOf('day').toDate(),
-          completions: [{completedOn: Date.now()}, {completedOn: Date.now()}]
-        }]
-      }).save(function(err, data){
-        testHabit = data;
-        done();
-      }));
-    });
-
-    it('creates a new interval if the completion falls outside the interval start and interval end timestamps', function(done){
+    it('creates a new interval if the completion falls outside the interval start and interval end timestamps', function(){
       chai.request('localhost:3000')
       .post('/api/completions/' + testHabit._id)
       .send({completedOn: moment(new Date()).add(2, 'days').toDate()})
-      .end(function(err, res) {
-        Habit.findOne({'_id': testHabit._id}, function(err, habit) {
-         // console.log(habit.intervals)
-          //It's already creating another interval by default. We don't want that.
+      .then(function() {
+        return Habit.findOne({'_id': testHabit._id})
+        .then(function(habit) {
           expect(habit.intervals.length).to.equal(2);
-          done();
         });
       });
+    });
 
+    it('does NOT create a new interval if the completion falls within the interval start and interval end timestamps', function(){
+      chai.request('localhost:3000')
+      .post('/api/completions/' + testHabit._id)
+      .send({completedOn: Date.now()})
+      .then(function(res) {
+        return Habit.findOne({'_id': testHabit._id})
+        .then(function(habit) {
+          expect(habit.intervals.length).to.equal(1);
+        });
+      });
+    });
+
+    it('changes an interval allComplete attribute to true when bonusFrequency goal is reached', function(){
+      return chai.request('localhost:3000')
+      .post('/api/completions/' + testHabit._id)
+      .send({completedOn: Date.now})
+      .then(function() {
+        return chai.request('localhost:3000')
+        .post('/api/completions/' + testHabit._id)
+        .send({completedOn: Date.now})
+        .then(function(){
+          return Habit.findOne({'_id': testHabit._id})
+          .then(function(habit){
+            //TODO: Assertion about the allcomplete attribute
+          })
+      });
     });
   });
+});
 });
 
